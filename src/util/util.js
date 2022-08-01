7b18cd06638f95f459828e5d843af256abae717f
jonathan <jonathanfagan@pursuit.org>
2022-08-01 17:12:28 -0400

    window debuging

--- src/util/util.js
+++ src/util/util.js
@@ -1,4 +1,5 @@
 export const apiUrl = () =>{
+    console.log(window.location.hostname)
     return window.location.hostname === "localhost"?
     "http://localhost:3001" : "https://cityhoops-backend.herokuapp.com"
 }
